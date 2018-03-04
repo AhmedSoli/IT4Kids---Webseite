09215a3f8c261fbc7d0f0a3a626567ab7752217f
Ahmed Soliman <=>
2018-03-04 15:29:38 +0100

    add team members and leaders now working

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -60,7 +60,7 @@
                     <div style="position: absolute;bottom: 25px;right: 25px;">
                         <a href="/lehrmaterial">Mehr Erfahren <i class="fa fa-external-link" aria-hidden="true"></i></a>
                     </div>
-                    <div style="position:absolute;bottom:20px;width:100%;">
+                    <div style="position:absolute;bottom:10px;width:100%;">
                         <span class="box blue fr"></span>
                         <span class="box red fl"></span>
                     </div>
