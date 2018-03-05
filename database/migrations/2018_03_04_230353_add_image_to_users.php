3ea3c69f5c6af20bb512dfe237efbd3fc9c03702
Ahmed Soliman <=>
2018-03-05 11:55:01 +0100

    fix articles displayed on welcome page for small to medium size screens

---
+++ database/migrations/2018_03_04_230353_add_image_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class AddImageToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('image');
+        });
+    }
+}
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -2,11 +2,21 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<h1 class="center">{{$post->title}}</h1>
-	</div>
-		<div class="row">
-		<div class="jumbotron" style="width:100%; height:300px;background-image: url('{{$post->image}}');background-size:cover;background-repeat: no-repeat;background-position: center center">
+		<div class="col-4">
+			<div class="row text-center">
+				<img src="https://it-for-kids.org/sites/default/files/styles/profil/public/ProfilLucas.jpg?itok=saD_jPvO"  style="max-height:150px;" class="img rounded-circle" alt="">
+			</div>
+			<div class="row">
+				
+			</div>
 		</div>
+		<div class="col-8">
+			<div class="jumbotron" style="width:100%; height:300px;background-image: url('{{$post->image}}');background-size:cover;background-repeat: no-repeat;background-position: center center">
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<h1 class="center">{{$post->title}}</h1>
 	</div>
 	<div hidden id="body">{{$post->body}}</div>
 	<div class="row" id="preview">
