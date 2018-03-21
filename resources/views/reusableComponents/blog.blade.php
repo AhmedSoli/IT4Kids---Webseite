50165dbf6713a390c43227784cadee826d70fbe9
Ahmed Soliman <=>
2018-03-21 14:22:09 +0100

    only show the latest 3 articles on the page

--- resources/views/reusableComponents/blog.blade.php
+++ resources/views/reusableComponents/blog.blade.php
@@ -10,7 +10,7 @@
         </div>
         <div class="row">
             @foreach($posts as $post)
-            <div class="col-md-4  col-6" style="margin-left:auto;margin-right:auto;">
+            <div class="col-4" style="margin-left:auto;margin-right:auto;">
                 <div class="card shadow">
                     <img class="card-img-top" src="{{$post->image}}" alt="Post Image" style="height:200px;">
                     <div class="card-body">
