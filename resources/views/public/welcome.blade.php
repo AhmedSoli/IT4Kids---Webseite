b4a6e6076b68f3989da7b034e797085ec72280cb
Ahmed Soliman <=>
2018-04-04 13:27:26 +0200

    .

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -2,22 +2,22 @@
 @section('content')
 <section id="main">
     <div class="container">
-        <div class="row" style="zoom:0.8;">
-            <div class="center">
-                <div class="row">
-                    <a href="https://impactchallenge.withgoogle.com/deutschland/charity/IT4Kids" data-toggle="tooltip"
-                        title="Erfahre mehr über Google Impact Challenge">
-                        Winners of <img src="{{ URL::to('/') }}/images/medal.png" alt="Medal" style="height:60px;">
-                    </a>
-                    <a onclick="toggle_celebration()" data-toggle="tooltip" title="Klicke hier für eine Überrashung">
-                        <img id="celebrate" src="{{ URL::to('/') }}/images/celebrate.png" alt="Celebration"
-                        style="height:20px; margin-bottom:-25px!important;">
-                    </a>
-                </div>
-            </div>
-        </div>
         <div class="row">
-            <div class="col-sm-6 text-center" style="padding-top: 100px;zoom:0.8">
+            <div class="col-sm-6 text-center" style="padding-top: 100px">
+                <div class="row" style="zoom:0.8;">
+                    <div class="center">
+                        <div class="row">
+                            <a href="https://impactchallenge.withgoogle.com/deutschland/charity/IT4Kids" data-toggle="tooltip"
+                                title="Erfahre mehr über Google Impact Challenge">
+                                Winners of <img src="{{ URL::to('/') }}/images/medal.png" alt="Medal" style="height:60px;">
+                            </a>
+                            <a onclick="toggle_celebration()" data-toggle="tooltip" title="Klicke hier für eine Überrashung">
+                                <img id="celebrate" src="{{ URL::to('/') }}/images/celebrate.png" alt="Celebration"
+                                style="height:20px; margin-bottom:-25px!important;">
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 <img src="{{ URL::to('/') }}/images/robot.gif" style="height:200px;" id="robot">
             </div>
             <div class="col-sm-6">
