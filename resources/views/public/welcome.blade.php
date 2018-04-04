fb695c3beaa8be3fa9ff79d93fdc5475259a799d
Ahmed Soliman <=>
2018-04-04 13:40:38 +0200

    .

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -3,58 +3,45 @@
 <section id="main">
     <div class="container">
         <div class="row">
-            <div class="col-6" style="zoom:0.8;">
-                <div class="row">
-                    <span class="center">
-                        <a href="https://impactchallenge.withgoogle.com/deutschland/charity/IT4Kids" data-toggle="tooltip"
-                            title="Erfahre mehr über Google Impact Challenge">
-                            Gewinner der <img src="{{ URL::to('/') }}/images/medal.png" alt="Medal" style="height:60px;">
-                        </a>
-                        <a onclick="toggle_celebration()" data-toggle="tooltip" title="Klicke hier für eine Überrashung">
-                            <img id="celebrate" src="{{ URL::to('/') }}/images/celebrate.png" alt="Celebration"
-                            style="height:20px;">
-                        </a>
-                    </span>
+            <div class="col-sm-6 text-center" style="padding-top: 50px;">
+                <div class="row pb-3">
+                    <div class="center">
+                        <div class="row">
+                            <a href="https://impactchallenge.withgoogle.com/deutschland/charity/IT4Kids" data-toggle="tooltip"
+                                title="Erfahre mehr über Google Impact Challenge">
+                                Winners of <img src="{{ URL::to('/') }}/images/medal.png" alt="Medal" style="height:60px;">
+                            </a>
+                            <a onclick="toggle_celebration()" data-toggle="tooltip" title="Klicke hier für eine Überrashung">
+                                <img id="celebrate" src="{{ URL::to('/') }}/images/celebrate.png" alt="Celebration"
+                                style="height:20px; margin-bottom:-25px!important;">
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div class="row pt-3">
-                    <img class="center" src="{{ URL::to('/') }}/images/robot.gif" style="height:200px;" id="robot">
-                </div>
-                <div class="row">
-                    <span class="center">
-                        <span style="font-size: 50px;line-height: 200%;opacity: 0.2;">❝</span>
-                        <p  style="margin-top:-40px;color:gray">
-                            <i>Everybody should learn how to  <strong style="color:black">code</strong>, because it teaches you how to  <strong style="color:black">think</strong>.
-                            <br>
-                            <span style="color:black;font-size: small;">- Steve Jobs</span>
-                            </i>
-                        </p>
-                    </span>
+                <div class="row text-center">
+                    <img src="{{ URL::to('/') }}/images/robot.gif" style="height:150px;margin-left:auto;margin-right:auto;" id="robot">
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-sm-6">
+                <div>
+                    <span style="font-size: 50px;line-height: 200%;opacity: 0.2;">❝</span>
+                    <p  style="margin-top:-40px;color:gray">
+                        <i>Everybody should learn how to  <strong style="color:black">code</strong>, because it teaches you how to  <strong style="color:black">think</strong>.
+                        <br>
+                        <span style="color:black;font-size: small;">- Steve Jobs</span>
+                        </i>
+                    </p>
+                </div>
                 @include('reusableComponents.console')
             </div>
         </div>
     </div>
 </section>
-<section id="cards" style="; margin-top:200px;">
+<section id="cards" style="margin-top:200px;background:url('/images/hypnotize.png');">
     <div class="container">
         <div class="row pb-3" >
-            <div class="col-md-6">
-                <div class="card text-center" id="lehrmaterial" style="height:250px;margin-top: -50px;background:url('/images/hypnotize.png');">
-                    <div>
-                        <span class="box red fl"></span>
-                        <span class="box blue fl"></span>
-                        <span>
-                            <span class="box blue fr"></span>
-                            <span class="box red fr"></span>
-                        </span>
-                        
-                    </div>
-                    <div>
-                        <span class="box blue fl"></span>
-                        <span class="box red fr"></span>
-                    </div>
+            <div class="col-6">
+                <div class="card text-center" id="lehrmaterial" style="height:250px;margin-top: -50px;">
                     <h3 class="heading" style="font-size: xx-large;padding-top:10%" id="lehrmaterialHeading">Lehrmaterial</h3>
                     <p id="lehrmaterialText" style="padding-top:5%;background-color:rgba(255, 255, 255, 0.67);">
                         Wir haben aus unseren Erfahrungen der letzten Jahre bereits Vorlagen und Empfehlungen abgeleitet, wie Informatikkurse und –AGs durchgeführt werden können. Jetzt kümmern wir uns darum dies in ein ganzheitliches Konzept zu gießen, mit dem Kurse unkompliziert an jeder Schule durchgeführt werden können.
@@ -62,61 +49,20 @@
                     <div style="position: absolute;bottom: 25px;right: 25px;">
                         <a href="/lehrmaterial">Mehr Erfahren <i class="fa fa-external-link" aria-hidden="true"></i></a>
                     </div>
-                    <div style="position:absolute;bottom:10px;width:100%;">
-                        <span class="box blue fr"></span>
-                        <span class="box red fl"></span>
-                    </div>
-                    <span style="width:100%;position:absolute;bottom:0px;">
-                        <span>
-                            <span class="box blue fl"></span>
-                            <span class="box red fl"></span>
-                        </span>
-                        <span>
-                            <span class="box red fr"></span>
-                            <span class="box blue fr"></span>
-                        </span>
-                    </span>
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="card card2 text-center" id="software" style="height:250px;margin-top: -50px;background:url('/images/hypnotize.png');">
-                    <div>
-                        <span class="box red fl"></span>
-                        <span class="box blue fl"></span>
-                        <span>
-                            <span class="box blue fr"></span>
-                            <span class="box red fr"></span>
-                        </span>
-                        
-                    </div>
-                    <div>
-                        <span class="box blue fl"></span>
-                        <span class="box red fr"></span>
-                    </div>
+            <div class="col-6">
+                <div class="card card2 text-center" id="software" style="height:250px;margin-top: -50px">
                     <h3 class="heading" style="font-size: xx-large;padding-top:10%" id="softwareHeading">Software</h3>
                     <p  id="softwareText" style="padding-top:5%;background-color:rgba(255, 255, 255, 0.67);">Unsere momentan laufenden Kurse arbeiten mit <a href="https://scratch.mit.edu/" style="color:red;">Scratch</a>, doch wir wollen die einfache Bedienung noch um einige Features wie integrierte Lernhilfen, Fortschritts-Tracking und Vorlagen in unserer eigenen <a href="https://github.com/IT4Kids/easypie" style="color:red">Software</a> erweitern.
-                </p>
+                    </p>
+                </div>
                 <div style="position: absolute;bottom: 25px;right: 25px;">
                     <a href="/software">Mehr Erfahren <i class="fa fa-external-link" aria-hidden="true"></i></a>
                 </div>
-                <div style="position:absolute;bottom:10px;width:100%;">
-                    <span class="box blue fr"></span>
-                    <span class="box red fl"></span>
-                </div>
-                <span style="width:100%;position:absolute;bottom:0px;">
-                    <span>
-                        <span class="box blue fl"></span>
-                        <span class="box red fl"></span>
-                    </span>
-                    <span>
-                        <span class="box red fr"></span>
-                        <span class="box blue fr"></span>
-                    </span>
-                </span>
             </div>
         </div>
     </div>
-</div>
 </section>
 @include('reusableComponents.blog')
 @include('reusableComponents.teamPhoto')
